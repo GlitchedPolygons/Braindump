eea1b537b3b1ce0ab449614a01374e3900104fa2
Raphael Beck <raphael.beck@glitchedpolygons.com>
2025-03-19 04:13:01 +0100

    Added a few comments; too tired to continue rn...

--- src/components/BraindumpEditor.vue
+++ src/components/BraindumpEditor.vue
@@ -1,10 +1,22 @@
 <script setup
         lang="ts">
 
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
+import {braindumpStore} from "@/braindump.ts";
+
+let editing = ref(true);
 
 onMounted(() =>
 {
+  if (braindumpStore.editedBraindump)
+  {
+    // todo: start in edit mode
+  }
+  else
+  {
+    // todo: start in reading mode
+  }
+  
   // todo
 });
 
